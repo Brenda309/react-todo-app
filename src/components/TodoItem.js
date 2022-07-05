d5dd5b74f2318c60a0d5660dde2f9eb3edab9f08
Brenda309 <brewihogora23@gmail.com>
2022-07-05 19:55:24 +0200

    handling events on buttons,input,checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import TodosList from './TodosList';
-
+import TodoContainer from './TodoContainer';
 
 function TodoItem(props) {
-  return <li>{props.todo.title}</li>;
+
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={props.todo.completed}
+        onChange={() => props.handleChangeProps(props.todo.id)}
+      />
+      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
+        Delete
+      </button>
+      {props.todo.title}
+    </li>
+  );
 }
 
 export default TodoItem;
