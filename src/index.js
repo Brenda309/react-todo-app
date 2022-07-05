c3f6cbf6139be6f8c8bed6eae8c54847153e3d20
Brenda309 <brewihogora23@gmail.com>
2022-07-05 21:48:46 +0200

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 // component file
 import TodoContainer from './components/TodoContainer';
 //stylesheet
-import "./App.css"
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />,
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
