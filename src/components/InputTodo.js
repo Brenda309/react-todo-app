4bbaad19ebde3ca493f0546a1d841bb5cee54875
Brenda309 <brewihogora23@gmail.com>
2022-07-11 22:04:51 +0200

    Change the class method into a function

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,10 +1,37 @@
 /* eslint-disable */
 import React, {useState} from "react"
 
-const InputTodo = () => {
-  console.log(useState('hello'))
-  return <div></div>
-}
+const InputTodo = (props) => {
+  const [title, setTitle] = useState('');
+
+  const onChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      props.addTodoProps(title);
+      setTitle('');
+    } else {
+      alert('Please write item');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
+  );
+};
 
 export default InputTodo
 
